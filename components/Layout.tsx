a9110200c9264d7a95519d8d50cb4fd84ffd32e3
Deepanshu Goyal <57024517+deepanshu363@users.noreply.github.com>
2024-03-02 09:34:54 +0530

    ui done , media quries in progress

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,12 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta
-        name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
-      />
-      <meta property="og:title" content={`${title} | Themeptation`} />
-      <meta
-        property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
-      />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+
+      <meta property="og:title" content={`${title}`} />
+
       <meta property="og:type" content="website" />
     </Head>
     {children}
